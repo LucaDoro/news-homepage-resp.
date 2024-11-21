2c5a69c71eaab4a548e3e681c7bbfce7445fe365
LucaDoro <luca.doro31@gmail.com>
2024-11-21 17:37:04 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// overlay = document.querySelector(".overlay");
+navMenuModal = document.querySelector(".nav-menu__modal");
+navMenu = document.querySelector(".nav-menu");
+
+btnOpen = document.querySelector(".burger-menu__open");
+btnClose = document.querySelector(".burger-menu__close");
+
+btnClose.addEventListener("click", function () {
+  navMenuModal.classList.add("hidden");
+  navMenu.classList.remove("hidden");
+  // overlay.classList.add("hidden");
+});
+
+btnOpen.addEventListener("click", function () {
+  navMenuModal.classList.remove("hidden");
+  navMenu.classList.add("hidden");
+
+  // overlay.classList.add("hidden");
+});
+
+/*
+TODO: remove this effect / or only do it for a certain media query (maybe see card-glow-effect js file)
+
+TODO: remove burger menu and display menu lists for desktop devices
+TODO: maybe see look up how to animate window ease in and out
+TODO: make background (overlay) darker
+*/
