9f85daae40b9d3a1839604f64f8a8b0d95d331cb
LucaDoro <luca.doro31@gmail.com>
2024-11-25 09:58:23 +0100

    Fixing functionality

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-// overlay = document.querySelector(".overlay");
+overlay = document.querySelector(".overlay");
 navMenuModal = document.querySelector(".nav-menu__modal");
 navMenu = document.querySelector(".nav-menu");
 
 btnOpen = document.querySelector(".burger-menu__open");
 btnClose = document.querySelector(".burger-menu__close");
 
-btnClose.addEventListener("click", function () {
-  navMenuModal.classList.add("hidden");
-  navMenu.classList.remove("hidden");
-  // overlay.classList.add("hidden");
-});
-
 btnOpen.addEventListener("click", function () {
   navMenuModal.classList.remove("hidden");
   navMenu.classList.add("hidden");
-
-  // overlay.classList.add("hidden");
+  overlay.classList.remove("hidden");
+});
+btnClose.addEventListener("click", function () {
+  navMenuModal.classList.add("hidden");
+  navMenu.classList.remove("hidden");
+  overlay.classList.add("hidden");
 });
 
 /*
